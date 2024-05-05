e3a6cf53d69f296c43d1fd1e0336342faaf4a843
lloydalcantara <lloyd@mazumausa.com>
2024-05-06 07:58:37 +0800

    Added a Banner section for the portfolio project

---
+++ src/components/Banner.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg"
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(0);
+    const toRotate = [ "Web Developer", "Laravel Developer", 'Backend Developer'];
+    const [text, setText] = useState('')
+    const [delta, setDelta] = useState(300 - Math.random() * 100)
+    const period = 2000
+    
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta)
+
+        return () => { clearInterval(ticker) }
+    }, [text])
+
+    const tick = () => {
+        let index = loopNum % toRotate.length;
+        let fullText = toRotate[index]
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2)
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true)
+            setDelta(period)
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false)
+            setLoopNum(loopNum + 1)
+            setDelta(500)
+        }
+    }
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome to my Portfolio</span>
+                        <h1>{`Hi I'm Lloyd `}<span className="wrap">{text}</span></h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae dui non lectus vehicula varius. Sed id gravida arcu. Nam odio ante, bibendum at libero id, lacinia pretium turpis. Sed venenatis sapien in urna feugiat sodales.</p>
+                        <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Img" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
